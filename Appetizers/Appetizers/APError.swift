b7136c459e96e591985b2b27fb5623bfb8d67517
Maxim Datskiy <datskiumax@gmail.com>
2024-03-18 01:33:25 -0700

    Add NetworkManager

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Maxim Datskiy on 3/18/24.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
