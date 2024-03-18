96620e1ed1c4c4de3fa3e22724a8ae17a3c4623d
Maxim Datskiy <datskiumax@gmail.com>
2024-03-18 00:36:25 -0700

    Add Appetizer Model

---
+++ Appetizers/Appetizers/Appetizer.swift
@@ -0,0 +1,39 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Maxim Datskiy on 3/17/24.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    
+    static let sampleAppetizer = Appetizer(
+        id: 0001,
+        name: "Test Appetizer",
+        description: "This is the description for my appetizer. It's yummy.",
+        price: 2.535,
+        imageURL: "",
+        calories: 14,
+        protein: 11,
+        carbs: 25
+    )
+    
+    static let appetizers = Array(repeating: sampleAppetizer, count: 5)
+}
