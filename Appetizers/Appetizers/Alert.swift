ffba1211896b77e43b98210acef1374df818c58f
Maxim Datskiy <datskiumax@gmail.com>
2024-03-18 19:50:54 -0700

    Add Errors and Alerts handling

---
+++ Appetizers/Appetizers/Alert.swift
@@ -0,0 +1,48 @@
+//
+//  Alert.swift
+//  Appetizers
+//
+//  Created by Maxim Datskiy on 3/18/24.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidData = AlertItem(
+        title: Text("Server Error"),
+        message: Text("The data received from the server was invalid. Please contact Support"),
+        dismissButton: .default(Text("OK"))
+    )
+
+    static let invalidResponse = AlertItem(
+        title: Text("Server Error"),
+        message: Text("Invalid response from the server. Please try again later or contact support."),
+        dismissButton: .default(Text("OK"))
+    )
+
+    static let invalidURL = AlertItem(
+        title: Text("Server Error"),
+        message: Text("There was an issue connecting to the server. If this persists, please contact support"),
+        dismissButton: .default(Text("OK"))
+    )
+
+    static let unableToComplete = AlertItem(
+        title: Text("Server Error"),
+        message: Text("Unable to complete your request at this time. Please check your internet connection."),
+        dismissButton: .default(Text("OK"))
+    )
+
+    static let invalidDeviceInput = AlertItem(
+        title: Text(""),
+        message: Text(""),
+        dismissButton: .default(Text("OK"))
+    )
+
+}
--- Appetizers/Appetizers/AppetizerList/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/AppetizerList/AppetizerListViewModel.swift
@@ -10,15 +10,29 @@ import Foundation
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
+                    
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                        
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                        
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                        
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
