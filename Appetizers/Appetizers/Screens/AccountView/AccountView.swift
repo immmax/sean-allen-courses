2f5e14849d151baa7d86f9f5c8677ae10c3af652
Maxim Datskiy <datskiumax@gmail.com>
2024-03-21 02:41:10 -0700

    Setup AccountView UI

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthday = Date.now
+    
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationStack {
-            Text("Account")
+            Form {
+                Section("Personal Info") {
+                    TextField("First Name", text: $firstName)
+                        .textInputAutocapitalization(.words)
+                    
+                    TextField("Last Name", text: $lastName)
+                        .textInputAutocapitalization(.words)
+                    
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    
+                    DatePicker("Birthday",
+                               selection: $birthday,
+                               displayedComponents: .date)
+                    
+                    Button("Save Changes") {
+                        print("Save: \(firstName) \(lastName)")
+                    }
+                    .tint(.brandPrimary)
+                }
+                
+                Section("Requests") {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }
+            }
                 .navigationTitle("☺️ Account")
         }
     }
