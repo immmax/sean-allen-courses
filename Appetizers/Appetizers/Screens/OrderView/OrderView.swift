c1d02f8de5fa09c37f7927b135b2b20724272eac
Maxim Datskiy <datskiumax@gmail.com>
2024-03-21 04:03:27 -0700

    Setup Order Screen UI, add Swipe gesture to delete items

--- Appetizers/Appetizers/Screens/OrderView/OrderView.swift
+++ Appetizers/Appetizers/Screens/OrderView/OrderView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems: [Appetizer] = Array(repeating: MockData.sampleAppetizer, count: 3)
+    
     var body: some View {
         NavigationStack {
-            Text("Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(.plain)
+                
+                Spacer()
+                
+                Button {
+                    print("Order placed")
+                } label: {
+                    APButton(title: "$\(orderItems[0].price, specifier: "%.2f") - Place Order")
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom, 25)
+                
+            }
                 .navigationTitle("🧾 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
--- Appetizers/Appetizers/Views/Buttons/APButton.swift
+++ Appetizers/Appetizers/Views/Buttons/APButton.swift
@@ -16,7 +16,7 @@ struct APButton: View {
             .font(.title3)
             .fontWeight(.semibold)
             .foregroundStyle(.white)
-            .padding()
+            .padding(.horizontal, 25)
     }
 }
 
