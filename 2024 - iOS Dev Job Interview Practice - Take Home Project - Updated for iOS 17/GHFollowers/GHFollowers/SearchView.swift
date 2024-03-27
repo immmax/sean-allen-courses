7c71e6d0973bac54a3f6507b3c32e51bb94f65e4
Maxim Datskiy <datskiumax@gmail.com>
2024-03-27 02:13:48 -0700

    Add SearchView UI

--- 2024 - iOS Dev Job Interview Practice - Take Home Project - Updated for iOS 17/GHFollowers/GHFollowers/SearchView.swift
+++ 2024 - iOS Dev Job Interview Practice - Take Home Project - Updated for iOS 17/GHFollowers/GHFollowers/SearchView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State private var username = ""
+    
     var body: some View {
         NavigationStack {
-            Text("Search View")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .navigationTitle("Search")
+            VStack(alignment: .center) {
+                Image(.ghLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding(.top, 80)
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(.gray.opacity(0.5), lineWidth: 2)
+                        .frame(width: 250, height: 50)
+                        .foregroundStyle(Color(.systemBackground))
+                        
+                    
+                    TextField(text: $username) {
+                        Text("Enter a username")
+                            .font(.title2)
+                            .multilineTextAlignment(.center)
+                    }
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .padding(.horizontal, 100)
+                }
+                .padding(.vertical)
+                    
+                Spacer()
+                
+                
+                Button {
+                    print(username)
+                } label: {
+                    Text("Search")
+                        .frame(width: 200, height: 50)
+                }
+                .padding(.bottom, 50)
+                .buttonStyle(.bordered)
+//                .controlSize(.large)
+                .tint(.green)
+            }
+            .background(Color(.systemBackground))
+            .navigationTitle("Search")
         }
     }
 }
