6367feaf86ca57e4b799a069bd7e9b6ccf5da20e
Maxim Datskiy <datskiumax@gmail.com>
2024-03-27 00:33:17 -0700

    Create and setup new project
    Add app icons and assets

---
+++ 2024 - iOS Dev Job Interview Practice - Take Home Project - Updated for iOS 17/GHFollowers/GHFollowers/GHFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersApp.swift
+//  GHFollowers
+//
+//  Created by Maxim Datskiy on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct GHFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
