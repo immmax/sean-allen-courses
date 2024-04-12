6574bd78f812580adb7054463bcae6bddc978a91
Maxim Datskiy <datskiumax@gmail.com>
2024-04-11 21:53:21 -0700

    Add FavoriteCell

---
+++ 2024 - iOS Dev Job Interview Practice - Take Home Project - Updated for iOS 17/GHFollowers/GHFollowers/Views/Cells/FavoriteCell.swift
@@ -0,0 +1,34 @@
+//
+//  FavoriteCell.swift
+//  GHFollowers
+//
+//  Created by Maxim Datskiy on 4/11/24.
+//
+
+import SwiftUI
+
+struct FavoriteCell: View {
+    
+    var favorite: Follower
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 24) {
+            
+            GFRemoteImage(urlString: favorite.avatarUrl)
+                .modifier(CellImageStyle())
+                .frame(height: 60)
+            
+            Text(favorite.login)
+                .font(.title.bold())
+                .multilineTextAlignment(.leading)
+                .lineLimit(1)
+                .frame(height: 40)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 12)
+    }
+}
+
+#Preview {
+    FavoriteCell(favorite: MockData.sampleFollower)
+}
