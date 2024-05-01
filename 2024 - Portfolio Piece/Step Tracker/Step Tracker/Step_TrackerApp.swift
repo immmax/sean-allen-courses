24170f2c2094261d9c63ad894f6bbf76f78ef5af
Maxim Datskiy <datskiumax@gmail.com>
2024-05-01 03:06:06 -0700

    Create new Xcode project
    Step Tracker

---
+++ 2024 - Portfolio Piece/Step Tracker/Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Maxim Datskiy on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
