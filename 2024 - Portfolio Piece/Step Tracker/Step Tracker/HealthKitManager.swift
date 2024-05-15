97dbba72b665ef2200ac338abcf01974c8ff34c8
Maxim Datskiy <datskiumax@gmail.com>
2024-05-14 20:20:44 -0700

    Add fetching steps and weight

--- 2024 - Portfolio Piece/Step Tracker/Step Tracker/HealthKitManager.swift
+++ 2024 - Portfolio Piece/Step Tracker/Step Tracker/HealthKitManager.swift
@@ -15,6 +15,41 @@ import Observation
     
     let types: Set = [HKQuantityType(.stepCount), HKQuantityType(.bodyMass)]
     
+    
+    func fetchSteps() async {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: .now)
+        let endDate = calendar.date(byAdding: .day, value: 1, to: today)!
+        let startDate = calendar.date(byAdding: .day, value: -28, to: endDate)
+        
+        let queryPredicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
+        let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.stepCount), predicate: queryPredicate)
+        let stepsQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate,
+                                                               options: .cumulativeSum,
+                                                               anchorDate: endDate,
+                                                               intervalComponents: .init(day: 1))
+        
+        let steps = try! await stepsQuery.result(for: store)
+    }
+    
+    
+    func fetchWeight() async {
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: .now)
+        let endDate = calendar.date(byAdding: .day, value: 1, to: today)!
+        let startDate = calendar.date(byAdding: .day, value: -28, to: endDate)
+        
+        let queryPredicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate)
+        let samplePredicate = HKSamplePredicate.quantitySample(type: HKQuantityType(.bodyMass), predicate: queryPredicate)
+        let weightQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate,
+                                                               options: .mostRecent,
+                                                               anchorDate: endDate,
+                                                               intervalComponents: .init(day: 1))
+        
+        let steps = try! await weightQuery.result(for: store)
+    }
+    
+    
 //    func addSimulatorData() async {
 //        var mockSamples: [HKQuantitySample] = []
 //        
