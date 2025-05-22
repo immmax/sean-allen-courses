9aa7a30a3cb2caff8ab4e6f0dfafa5f6a32c1ba4
Maxim Datskiy <datskiumax@gmail.com>
2025-05-21 21:14:16 -0700

    MapView - Show the Map

--- 2025 - DubDubGrub/DubDubGrub/DubDubGrub/LocationMapView.swift
+++ 2025 - DubDubGrub/DubDubGrub/DubDubGrub/LocationMapView.swift
@@ -5,11 +5,28 @@
 //  Created by Maxim Datskiy on 5/21/25.
 //
 
+import MapKit
 import SwiftUI
 
 struct LocationMapView: View {
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    
     var body: some View {
-        Text("Map")
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image(.ddgMapLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: 10)
+                 
+                Spacer()
+            }
+        }
     }
 }
 
